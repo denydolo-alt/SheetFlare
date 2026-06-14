60954196c40c6fdaa38dbb71a0e0658470c3918a
denydolo-alt <deny.dolo@gmail.com>
2026-06-14 20:06:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 // PENGATURAN DATABASE & SISTEM UTAMA
 // ==========================================
 
-// Biarkan SCRIPT_URL menunjuk ke domain Anda sendiri
+// Biarkan SCRIPT_URL menunjuk ke domain Bos sendiri
 // Cloudflare _worker.js yang akan menangani sisanya
 const SCRIPT_URL = window.location.origin + "/";
 window.SCRIPT_URL = SCRIPT_URL; 
 
-const SHEET_ID "1wmksvipdpkEZFLrCzciR-r3AMD-ypTvdcMACRyywueI/edit?usp=drivesdk";
+const SHEET_ID = "1wmksvipdpkEZFLrCzciR-r3AMD-ypTvdcMACRyywueI/edit?usp=drivesdk";
 window.SHEET_ID = SHEET_ID;
 
 // ==========================================
